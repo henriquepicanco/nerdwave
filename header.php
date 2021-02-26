63fa3510b3c7e5dc5883205ed675afe3012cb9a9
Henrique Picanço <henriquepicanco97@gmail.com>
2021-02-26 15:33:41 -0300

    Fix: H1 use at homepage

--- header.php
+++ header.php
@@ -39,7 +39,23 @@
 					</div>
 
 					<div class="col-sm-4 col-4">
-						<?php get_template_part( 'template-parts/header/site-branding' ); ?>
+						<div class="site-branding">
+							<?php
+							if ( is_front_page() && is_home() ) :
+								?>
+								<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+								<?php
+							else :
+								?>
+								<p class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+								<?php
+							endif;
+							?>
+							
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_html( 'Homepage', 'nerdwave' ); ?>" rel="home">
+								<?php get_template_part( 'template-parts/brand/logo' ); ?>
+							</a>
+						</div>
 					</div>
 
 					<div class="col-sm-4 col-4">
